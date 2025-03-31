e4158c849ec1730943d5586d8edfecc77e33ee8d
Park, Daekyeong <pdk02kkw71@naver.com>
2025-03-31 19:30:34 +0900

    데스크탑 전용 페이지로 변경

--- src/App.js
+++ src/App.js
@@ -5,8 +5,21 @@ import MainPageFood from './pages/MainpageFood';
 import MainPageMap from './pages/MainpageMap';
 import './assets/fonts/fonts.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
+import './App.css';
 
 function App() {
+  const isMobile = useMediaQuery({ query: '(max-width: 1000px' });
+  if(isMobile) {
+    return (
+      <div className='mobile-display'>
+        <div style={{flexDirection:"row", gap:"0", justifyContent:"center", alignItems:"center"}}>
+            데스크톱만 가능합니다.
+        </div>
+      </div>
+    )
+  }
+
   return (
     <Router>
       <Routes>
