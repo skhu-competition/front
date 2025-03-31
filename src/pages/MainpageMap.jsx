249c872d7fc1fc4b7f550b851ed1f92514ad1a37
Park, Daekyeong <pdk02kkw71@naver.com>
2025-03-31 15:59:22 +0900

    지도 요소 추가

--- src/pages/MainpageMap.jsx
+++ src/pages/MainpageMap.jsx
@@ -1,6 +1,57 @@
 import "./css/Mainpage-map.css"
 import logo from "../assets/logo.png";
+import { useEffect, useRef } from "react";
+
+const { naver } = window;
+
 const MainPageMap = () => {
+
+    const container = useRef(null);
+    useEffect(() => {
+        const skhu_position = new naver.maps.LatLng(37.487700, 126.825400);
+
+        const options = {
+            center: skhu_position,
+            zoom: 17,
+            zoomControl: true,
+            zoomControlOptions: {
+                style: naver.maps.ZoomControlStyle.SMALL,
+                position: naver.maps.Position.TOP_RIGHT
+            }
+        };
+
+        const map = new naver.maps.Map(container.current, options);
+
+        const dawon_guksu_position = new naver.maps.LatLng(37.489306, 126.825079);
+        const dawon_guksu_marker = new naver.maps.Marker({
+            position: dawon_guksu_position,
+            map: map
+        })
+        const dawon_guksu_content = [
+            '<div style="color:black">',
+            '   <h3>다원국수</h3>',
+            '   <hr/>',
+            '   <p>국수 맛집.<br />',
+            '   </p>',
+            '</div>'
+        ].join('');
+
+        const infowindow = new naver.maps.InfoWindow({
+            content: dawon_guksu_content,
+            maxWidth: 140,
+            backgroundColor: "#ffffff",
+            borderColor: "#2db400",
+            borderWidth: 4,
+            anchorSize: new naver.maps.Size(30, 30),
+            anchorSkew: true,
+            anchorColor: "#eee",
+            pixelOffset: new naver.maps.Point(20, -20)
+        });
+
+        naver.maps.Event.addListener(dawon_guksu_marker, "click", function() {
+            infowindow.open(map, dawon_guksu_marker);
+        })
+    })
     return(
         <div className="wrap">
             <img src={logo} alt="logo" className="title"/>
@@ -11,9 +62,7 @@ const MainPageMap = () => {
               <li></li>
             </ul>
             <div className="bg">
-                <ul className="category1">
-                    <li></li>
-                </ul>
+                <div className="map" ref={ container }></div>
             </div>
             
         </div>
